2a80f7c278a33c268125423537a899065038a266
Sinisig <Shacklebird1448@gmail.com>
2023-01-07 22:59:57 -0500

    Sorted files by contained dating information

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-fn date_directory(
+// Dates every file in a directory and sorts
+// the contents from oldest to newest.
+fn date_directory_sorted(
    path : & str,
 ) -> Result<Vec<(String, Vec<dacom::Date>)>, Box<dyn std::error::Error>> {
    let mut dates = Vec::new();
@@ -11,7 +13,7 @@ fn date_directory(
 
       if ent.file_type()?.is_dir() == true {
          // Recursively search subdirectories
-         let mut sub_info = date_directory(path)?;
+         let mut sub_info = date_directory_sorted(path)?;
          dates.append(&mut sub_info);
 
       } else {
@@ -30,6 +32,33 @@ fn date_directory(
       }
    }
 
+   // Sort all the files in order using the oldest date
+   dates.sort_unstable_by(|a, b| {
+      use std::cmp::Ordering::*;
+
+      // Only care about dating info
+      let a = &a.1;
+      let b = &b.1;
+
+      // First try matching by oldest file date
+      return match a.first().unwrap().cmp(b.first().unwrap()) {
+         Less     => Less,
+         Greater  => Greater,
+         Equal    => {
+            // Oldest file dates match, try comparing
+            // newest file date
+            match a.last().unwrap().cmp(b.last().unwrap()) {
+               Less     => Less,
+               Greater  => Greater,
+               Equal    => {
+                  // Give up.  Just use counts
+                  a.len().cmp(&b.len())
+               },
+            }
+         },
+      };
+   });
+
    return Ok(dates);
 }
 
@@ -39,13 +68,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
    for arg in args {
       println!("Seaching directory {arg}");
-      let search_results = date_directory(&arg)?;
+      let search_results = date_directory_sorted(&arg)?;
 
       println!("Done! Here are the results:");
+      println!("");
       for (file, dates) in &search_results {
-         println!("{file}:");
+         println!("{file}");
          for date in dates {
-            println!("\t{date}");   
+            println!("   {date}");   
          }
          println!("");
       }

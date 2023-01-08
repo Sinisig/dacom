ae05b5eb2f8ca4fde1def4394a5a7265a8970d3a
Sinisig <Shacklebird1448@gmail.com>
2023-01-07 22:08:35 -0500

    Wrote crude, error-prone, and messy first implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
-fn date_file(file : std::fs::File) -> Vec<dacom::Date> {
-   return Vec::new();
+fn date_directory(
+   path : & str,
+) -> Result<Vec<(String, Vec<dacom::Date>)>, Box<dyn std::error::Error>> {
+   let mut dates = Vec::new();
+
+   // Iterate for every element in the current directory
+   for ent in std::fs::read_dir(path)? {
+      let ent  = ent?;
+      let path = ent.path();
+      let path = path.to_str().unwrap();
+
+      if ent.file_type()?.is_dir() == true {
+         // Recursively search subdirectories
+         let mut sub_info = date_directory(path)?;
+         dates.append(&mut sub_info);
+
+      } else {
+         // Read the file text
+         let text = match std::fs::read_to_string(&path) {
+            Ok(v)    => v,
+            Err(_)   => String::new(),
+         };
+
+         // Search for all dates in the text
+         let found_dates = dacom::Date::from_string(&text);
+
+         // Add the result to the running total
+         dates.push((String::from(path), found_dates));
+      }
+   }
+
+   return Ok(dates);
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-   for arg in std::env::args() {
-      println!("{arg}");
+   let mut args = std::env::args();
+   args.next();   // Skips the executable path
+
+   for arg in args {
+      println!("Seaching directory {arg}");
+      let search_results = date_directory(&arg)?;
+
+      println!("Done! Here are the results:");
+      for (file, dates) in &search_results {
+         println!("{file}:");
+         for date in dates {
+            println!("\t{date}");
+         }
+      }
+
+      println!("");
    }
 
    return Ok(());

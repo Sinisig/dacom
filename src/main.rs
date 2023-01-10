096fc532ea1a1f06dd3850f4548c89f5e0a02443
Sinisig <Shacklebird1448@gmail.com>
2023-01-09 21:41:40 -0500

    Created new main to use updated library

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
    let args = dacom::Args::new(std::env::args());
    
    // Load dates from files
+   if args.verbose() {println!(
+      "Aggregating file dating information starting at {}...",
+      args.input_file(),
+   )};
    let data = dacom::FileDateAggregate::new(
-      &args.input_file(),
-      |file| println!("Searching {file}"),
+      args.input_file(),
+      |_| {},
    )?;
 
-   // Test code for new stuff
-   println!("{}", data.create_report());
+   // Create a data summary report
+   if args.verbose() {println!(
+      "Creating statistical report of the collected data...",
+   )};
+   let data = data.create_report();
+
+   if let Some(file) = args.output_file() {
+      if args.verbose() {println!(
+         "Writing report to {file}...",
+      )};
+      std::fs::write(file, data.to_string())?;
+   } else {
+      print!("{data}");
+   }
 
    // Return success
    return Ok(());

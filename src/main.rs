abdff537722daa30ee798e602be5f9c38d4880e7
Sinisig <Shacklebird1448@gmail.com>
2023-01-10 15:21:23 -0500

    Add basic driver code in main for rewrite of crate::collect

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
    // Parse command-line arguments
    let args = dacom::Args::new(std::env::args());
    
-   // TODO: Everything lol
+   // Collect dates from files
+   let data = dacom::FileAggregateDateSet::new_recursive_with(
+      &args.input_file(),
+      |path| println!("Searched {}", path.to_str().unwrap()),
+   )?;
    
+   // Print the results
+   for file in data.as_slice() {
+      println!("{}", file.path().to_str().unwrap());
+      for date in file.dates().as_slice() {
+         println!("   {date}");
+      }
+      println!("");
+   }
+
    // Return success
    return Ok(());
 }

999666aa0ccf136a9593d1792cd9366f9e08d057
Sinisig <Shacklebird1448@gmail.com>
2023-01-10 22:02:43 -0500

    Fully finished rewriting main and restoring complete functionality after the refactor

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
    let args = dacom::Args::new(std::env::args());
    
    // Collect dates from files
+   if args.verbose() {println!(
+      "Starting search for and collection of dates starting at {}...",
+      &args.input_file(),
+   )};
    let data = dacom::FileAggregateDateSet::new_recursive_with(
       &args.input_file(),
-      |path| println!("Searched {}", path.to_str().unwrap()),
+      if args.verbose() {
+         |path : & std::path::Path| {
+            println!("Searching {}...", path.to_str().unwrap());
+         }
+      } else {
+         |_ : & std::path::Path| {}
+      },
    )?;
    
    // Create a data report
+   if args.verbose() {println!(
+      "Analyzing found dates and creating a report...",
+   )};
    let data = data.create_report();
 
    // Send the data to the appropriate file stream
    if let Some(path) = &args.output_file() {
+      if args.verbose() {println!(
+         "Writing results to {path}...",
+      )};
       std::fs::write(path, data.to_string())?;
    } else {
       print!("{data}");
    }
 
    // Return success
+   if args.verbose() {println!(
+      "Exiting program successfully...",
+   )};
    return Ok(());
 }
 
